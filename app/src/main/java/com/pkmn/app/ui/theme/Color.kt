ed56f367c6e81e97cbc6e8e8253dddd6411422f5
hangnadi <hangnadi@gmail.com>
2025-10-17 14:01:40 +0700

    Color.kt: add color library

--- app/src/main/java/com/pkmn/app/ui/theme/Color.kt
+++ app/src/main/java/com/pkmn/app/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ColorWhite = Color(0xFFFFFFFF)
+val ColorBlack = Color(0xFF000000)
+val Black70 = Color(0xFF323232)
+
+val Grey40 = Color(0xFF8B8B97)
+val Gray95 = Color(0xFFF7F7F7)
+val Gray10 = Color(0xFF16162E)
+
+val Alert_Success = Color(0xFF00AC56)
+val Alert_Error = Color(0xFFFF1F7D)
+val Alert_Warning = Color(0xFFF5BF3D)
