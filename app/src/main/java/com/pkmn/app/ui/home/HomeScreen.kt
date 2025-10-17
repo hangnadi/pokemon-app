1a843f41a1de97318f75d33fe2037087ae2437df
hangnadi <hangnadi@gmail.com>
2025-10-17 14:42:36 +0700

    HomeScreen.kt: init home screen

---
+++ app/src/main/java/com/pkmn/app/ui/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.pkmn.app.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+
+}
