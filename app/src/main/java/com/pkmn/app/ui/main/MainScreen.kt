f5484684bc54eb159502463e6581d61a13cff452
hangnadi <hangnadi@gmail.com>
2025-10-19 18:21:37 +0700

    MainScreen.kt.kt: implement topbar

--- app/src/main/java/com/pkmn/app/ui/main/MainScreen.kt
+++ app/src/main/java/com/pkmn/app/ui/main/MainScreen.kt
@@ -1,28 +1,46 @@
 package com.pkmn.app.ui.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.pkmn.app.R
 import com.pkmn.app.navigation.AppRoute
 import com.pkmn.app.ui.home.HomeScreen
 import com.pkmn.app.ui.profile.ProfileScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
-    rootNavHostController: NavHostController
+    rootNavHostController: NavHostController,
+    modifier: Modifier = Modifier
 ) {
     val navController = rememberNavController()
     val currentBackStack by rootNavHostController.currentBackStackEntryAsState()
@@ -31,6 +49,28 @@ fun MainScreen(
     }
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                title = { Text(
+                    stringResource(id = R.string.app_name)
+                ) },
+                actions = {
+                    IconButton(
+                        onClick = { /* @TODO LOGOUT Feature */ }
+                    ) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "Log Out"
+                        )
+                    }
+                }
+            )
+        },
         bottomBar = {
             BottomNavBar(
                 items = getNavigationItems(),
