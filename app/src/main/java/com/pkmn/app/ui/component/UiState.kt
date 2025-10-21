89ca773c7ac8666b4bf770604795bcb47c5d9175
hangnadi <hangnadi@gmail.com>
2025-10-21 15:08:50 +0700

    UiState.kt : create common class for state

---
+++ app/src/main/java/com/pkmn/app/ui/component/UiState.kt
@@ -0,0 +1,8 @@
+package com.pkmn.app.ui.component
+
+sealed class UiState {
+    object Idle : UiState()
+    object Loading : UiState()
+    object Success : UiState()
+    data class Error(val message: String) : UiState()
+}
