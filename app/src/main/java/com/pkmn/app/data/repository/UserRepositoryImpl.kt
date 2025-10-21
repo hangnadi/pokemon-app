e2e8cfb7f0452812ca27902af94e4c6dee5759d1
hangnadi <hangnadi@gmail.com>
2025-10-21 09:26:07 +0700

    UserRepository.kt: add function login and session CRUD

--- app/src/main/java/com/pkmn/app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/pkmn/app/data/repository/UserRepositoryImpl.kt
@@ -1,19 +1,35 @@
 package com.pkmn.app.data.repository
 
+import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.pkmn.app.data.remote.database.UserDao
 import com.pkmn.app.data.remote.database.UserEntity
 import com.pkmn.app.domain.repository.UserRepository
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val userDao: UserDao
+    private val userDao: UserDao,
+    private val sp: SharedPreferences,
 ) : UserRepository {
-    override suspend fun hasLoggedInUser(): Boolean {
-        /**
-         * @TODO
-         * Implement with room
-         */
-        return false
+
+    companion object {
+        private const val KEY_LOGGED_IN_USER = "logged_in_user"
+    }
+
+    override suspend fun setUserSession(session: String) {
+        sp.edit { putString(KEY_LOGGED_IN_USER, session) }
+    }
+
+    override suspend fun getUserSession(): String? {
+        return sp.getString(KEY_LOGGED_IN_USER, "")
+    }
+
+    override suspend fun clearUserSession() {
+        sp.edit { remove(KEY_LOGGED_IN_USER) }
+    }
+
+    override suspend fun hasUserSession(): Boolean {
+        return sp.contains(KEY_LOGGED_IN_USER)
     }
 
     override suspend fun registerUser(userEntity: UserEntity): Boolean {
@@ -21,9 +37,13 @@ class UserRepositoryImpl @Inject constructor(
         return result != -1L
     }
 
+    override suspend fun loginUser(email: String, password: String): Boolean {
+        val result = userDao.loginUser(email, password)
+        return result != null
+    }
+
     override suspend fun getUserByEmail(email: String): UserEntity? {
         return userDao.getUserByEmail(email)
     }
-
 
 }
--- app/src/main/java/com/pkmn/app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/pkmn/app/domain/repository/UserRepository.kt
@@ -3,9 +3,12 @@ package com.pkmn.app.domain.repository
 import com.pkmn.app.data.remote.database.UserEntity
 
 interface UserRepository {
-    suspend fun hasLoggedInUser(): Boolean
+    suspend fun setUserSession(session: String)
+    suspend fun getUserSession():String?
+    suspend fun clearUserSession()
+    suspend fun hasUserSession(): Boolean
     suspend fun registerUser(userEntity: UserEntity): Boolean
-
+    suspend fun loginUser(email: String, password:String): Boolean
     suspend fun getUserByEmail(email: String): UserEntity?
 
 }
